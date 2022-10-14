1e1dbb8913c8cc6d549541963bf35952fd48f1f1
rishianand39 <rishi.rn818@gmail.com>
2022-10-14 19:40:48 +0530

    share-gif

--- comment__app/api/src/configs/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require("mongoose")
-const dotenv=require("dotenv")
-dotenv.config()
-
- const connect=()=>{
-    return mongoose.connect(process.env.MONGO_URI)
-}
-module.exports=connect
